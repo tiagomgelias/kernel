6ff1505ce872876e986d405829cd639de87cb0e8
Paulo Elias <paulo.cgelias@gmail.com>
2017-12-12 18:31:09 +0000

    Created a new profile called ApiProfile

---
+++ src/Profiles/ApiProfile.php
@@ -0,0 +1,45 @@
+<?php
+namespace Electro\Profiles;
+
+use Electro\DependencyInjection\Injector;
+use Electro\Interfaces\ModuleInterface;
+use Electro\Interfaces\ProfileInterface;
+use Electro\Kernel\Services\Kernel;
+use Electro\WebServer\WebBootloader;
+
+/**
+ * A configuration profile tailored for web application APIs.
+ *
+ */
+class ApiProfile implements ProfileInterface
+{
+	public function getBootloaderClass ()
+	{
+		return WebBootloader::class;
+	}
+
+	public function getExcludedModules ()
+	{
+		return [];
+	}
+
+	public function getIncludedModules ()
+	{
+		return [];
+	}
+
+	public function getInjector ()
+	{
+		return new Injector;
+	}
+
+	public function getKernelClass ()
+	{
+		return Kernel::class;
+	}
+
+	public function getName ()
+	{
+		return str_segmentsLast (static::class, '\\');
+	}
+}
