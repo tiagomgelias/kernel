5ca2f9dcc7193f9ca27ed6f161860d28c77e958f
Cláudio Silva <claudio.silva@impactwave.com>
2018-04-20 12:30:29 +0100

    Reformat.

--- src/Profiles/ApiProfile.php
+++ src/Profiles/ApiProfile.php
@@ -2,44 +2,42 @@
 namespace Electro\Profiles;
 
 use Electro\DependencyInjection\Injector;
-use Electro\Interfaces\ModuleInterface;
 use Electro\Interfaces\ProfileInterface;
 use Electro\Kernel\Services\Kernel;
 use Electro\WebServer\WebBootloader;
 
 /**
  * A configuration profile tailored for web application APIs.
- *
  */
 class ApiProfile implements ProfileInterface
 {
-	public function getBootloaderClass ()
-	{
-		return WebBootloader::class;
-	}
+  public function getBootloaderClass ()
+  {
+    return WebBootloader::class;
+  }
 
-	public function getExcludedModules ()
-	{
-		return [];
-	}
+  public function getExcludedModules ()
+  {
+    return [];
+  }
 
-	public function getIncludedModules ()
-	{
-		return [];
-	}
+  public function getIncludedModules ()
+  {
+    return [];
+  }
 
-	public function getInjector ()
-	{
-		return new Injector;
-	}
+  public function getInjector ()
+  {
+    return new Injector;
+  }
 
-	public function getKernelClass ()
-	{
-		return Kernel::class;
-	}
+  public function getKernelClass ()
+  {
+    return Kernel::class;
+  }
 
-	public function getName ()
-	{
-		return str_segmentsLast (static::class, '\\');
-	}
+  public function getName ()
+  {
+    return str_segmentsLast (static::class, '\\');
+  }
 }
